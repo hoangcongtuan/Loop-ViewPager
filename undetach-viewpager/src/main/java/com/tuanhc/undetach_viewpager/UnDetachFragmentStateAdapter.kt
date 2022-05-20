fd35d738fcab60a6ef145f247920227141cef51a
Tuan Hoang Cong <congtuanspm97@gmail.com>
2022-05-20 17:00:34 +0700

    remove unsued

--- undetach-viewpager/src/main/java/com/tuanhc/undetach_viewpager/UnDetachFragmentStateAdapter.kt
+++ undetach-viewpager/src/main/java/com/tuanhc/undetach_viewpager/UnDetachFragmentStateAdapter.kt
@@ -1,31 +1,8 @@
 package com.tuanhc.undetach_viewpager
 
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.adapter.FragmentViewHolder
 
 abstract class UnDetachFragmentStateAdapter(private val fa: FragmentActivity) : com.example.customviewpager2.FragmentStateAdapter(fa) {
-    /*abstract override fun getItemCount(): Int*/
-
-   /* override fun createFragment(position: Int): Fragment {
-        TODO("Not yet implemented")
-    }*/
-
-    /*override fun createFragment(position: Int): Fragment {
-        TODO("Not yet implemented")
-    }*/
-
-
-    /*override fun onBindViewHolder(
-        holder: FragmentViewHolder,
-        position: Int,
-        payloads: MutableList<Any>
-    ) {
-        super.onBindViewHolder(holder, position, payloads)
-        holder.setIsRecyclable(false)
-    }*/
-
    /* override fun onBindViewHolder(
         holder: com.example.customviewpager2.FragmentViewHolder,
         position: Int,
